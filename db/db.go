32111c9380cd8d49be4c5b0bc32d8dbecb7fd628
Yoiner Castillo Martinez <yoiner.castillo@globant.com>
2020-01-30 16:48:33 -0500

    crud

--- db/db.go
+++ db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 )
@@ -12,25 +13,32 @@ var mu sync.Mutex
 
 //func Create(key <-chan string, data <-chan interface{}) bool {
 
-func Create(key string, data interface{}) bool {
+func Create(key string, data interface{}) (bool, error) {
 	creo := false
 	mu.Lock()
 	//llave := <-key
 	if Datos[key] == nil {
 		Datos[key] = data
 		creo = true
+	} else {
+		mu.Unlock()
+		return false, errors.New("Error, No se puede crear, la clave ya existe")
 	}
 	mu.Unlock()
-	return creo
+	return creo, nil
 }
 
 //strconv.Itoa( convierto int a string
-func Retrieve(key string) interface{} {
+func Retrieve(key string) (interface{}, error) {
 	//func Retrieve(key <-chan string) interface{} {
-	return Datos[key]
+
+	if Datos[key] == nil {
+		return nil, errors.New("No se encontró información con la clave recibida")
+	}
+	return Datos[key], nil
 }
 
-func Update(key string, data interface{}) bool {
+func Update(key string, data interface{}) (bool, error) {
 	//func Update(key <-chan string, data <-chan interface{}) bool {
 
 	actualizo := false
@@ -39,12 +47,15 @@ func Update(key string, data interface{}) bool {
 	if Datos[key] != nil {
 		Datos[key] = data
 		actualizo = true
+	} else {
+		mu.Unlock()
+		return false, errors.New("No se encontró información con la clave recibida")
 	}
 	mu.Unlock()
-	return actualizo
+	return actualizo, nil
 }
 
-func Delete(key string) bool {
+func Delete(key string) (bool, error) {
 	//func Delete(key <-chan string) bool {
 
 	elimino := false
@@ -53,9 +64,12 @@ func Delete(key string) bool {
 	if Datos[key] != nil {
 		delete(Datos, key)
 		elimino = true
+	}else{
+		mu.Unlock()
+		return false, errors.New("No se encontró información con la clave recibida")
 	}
 	mu.Unlock()
-	return elimino
+	return elimino, nil
 }
 
 func PrintDatos() {
--- db/db_test.go
+++ db/db_test.go
@@ -2,24 +2,32 @@ package db
 
 import (
 	"fmt"
-	"testing"
 	"github.com/yoiner-castillo-globant/GBcamp/db"
+	"testing"
 )
 
 func TestCreate(t *testing.T) {
-	db.Create("test1", "50")
+	resp, err := db.Create("test1", "50")
+	if !resp {
+		t.Errorf("Create was incorrect, got this error: %s", err)
+	}
+
 	length := len(db.Datos)
 
-	if length > 1 {
+	if length == 1 {
 		t.Errorf("Create was incorrect, got: %d, want: %d.", length, 1)
 	}
 }
 
 func TestRetrieve(t *testing.T) {
 	db.Create("test2", "Funciona")
-	var x interface{} = db.Retrieve("test2")
+	x, err := db.Retrieve("test2")
 	dato := fmt.Sprintf("%v", x)
 
+	if err != nil {
+		t.Errorf("Retrieve was incorrect, got this error: %s, ", err)
+
+	}
 	if dato != "Funciona" {
 		t.Errorf("Retrieve was incorrect, got: %s, want: %s.", dato, "Funciona")
 	}
@@ -27,10 +35,14 @@ func TestRetrieve(t *testing.T) {
 
 func TestUpdate(t *testing.T) {
 	db.Create("test3", "Funciona")
-	db.Update("test3", "Funciona2")
-	var x interface{} = db.Retrieve("test3")
+	val, err := db.Update("test3", "Funciona2")
+	x, _ := db.Retrieve("test3")
 	dato := fmt.Sprintf("%v", x)
 
+	if !val {
+		t.Errorf("Retrieve was incorrect, got this error: %s, ", err)
+
+	}
 	if dato != "Funciona2" {
 		t.Errorf("Update was incorrect, got: %s, want: %s.", dato, "Funciona2")
 	}
@@ -39,10 +51,16 @@ func TestUpdate(t *testing.T) {
 func TestDelete(t *testing.T) {
 	db.Create("test4", "Eliminar Test")
 	cantidadAnterior := len(db.Datos)
-	db.Delete("test4")
+	val, err := db.Delete("test4")
+//Incorrect
+	//	val, err := db.Delete("test")
+
 	cantidadPosterior := len(db.Datos)
 	cantidadAnterior--
 
+	if !val {
+		t.Errorf("Update was incorrect, got this error: %s", err)
+	}
 	if cantidadAnterior != cantidadPosterior {
 		t.Errorf("Update was incorrect, got: %d, want: %d.", cantidadAnterior, cantidadPosterior)
 	}
